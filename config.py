35563a901f9d98772222ee16e6f9887aacfe85c3
ak4422920 <ak4422920@gmail.com>
2025-05-10 13:02:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
 from typing import List
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = os.environ.get("API_ID", "29171167")
+API_HASH = os.environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+ADMIN = int(os.environ.get("ADMIN", "7251898668 1337857036"))
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002190352334"))
+NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "True").lower() == "true"  # Set "True" For accept new requests
 
 DB_URI = os.environ.get("DB_URI", "")
 DB_NAME = os.environ.get("DB_NAME", "")
 
-IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100******** -100*********").split())) # Add Multiple channel ids
+IS_FSUB = os.environ.get("IS_FSUB", "True").lower() == "true"  # Set "True" For Enable Force Subscribe
+AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002442422204 -1001785093771").split())) # Add Multiple channel ids
