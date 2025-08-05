3da9c1b29df496951a3042735f443b24c99fb708
ak4422920 <ak4422920@gmail.com>
2025-08-05 20:05:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ADMIN = int(os.environ.get("ADMIN", "8371607189"))
 PICS = (os.environ.get("PICS", "https://i.ibb.co/KzxDQL7n/photo-2025-08-05-13-46-11-7535097427330596884.jpg")).split()
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002210359926"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002190352334"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "True").lower() == "true"  # Set "True" For accept new requests
 
 DB_URI = os.environ.get("DB_URI", "")
